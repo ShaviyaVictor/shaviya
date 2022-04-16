4ef656bbb9d362a6966ea36b12c1911d59eb024c
ShaviyaVictor <shaviyavictor@gmail.com>
2022-04-16 06:39:43 +0300

    Add body color switch for the light and dark modes

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -26,12 +26,17 @@ function setActiveStyle(color)
       style.setAttribute('disabled', 'true');
     }
   })
-  // localStorage.setItem('color', color);
-  // changeColor();
 }
 
 // ===== light and dark mode themes =====
 const dayNight = document.querySelector('.day-night');
+
+dayNight.addEventListener('click', () => {
+  dayNight.querySelector('i').classList.toggle('fa-sun');
+  dayNight.querySelector('i').classList.toggle('fa-moon');
+  document.body.classList.toggle('dark');
+});
+
 window.addEventListener('load', () => {
   if (document.body.classList.contains('dark'))
   {
