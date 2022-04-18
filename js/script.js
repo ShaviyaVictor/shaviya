e31c96a04074d8e99e013a1c3376829c704f7498
ShaviyaVictor <shaviyavictor@gmail.com>
2022-04-18 07:42:16 +0300

    The email fuctionality would expose my smtp personal credentials. So I opted out of that option. It is a dummy now alerting users to reach me via other social media handles.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
+// Email sending script
+var sendBtn = document.getElementById('onClick');
 
+sendBtn.addEventListener('click', function(e) {
+  e.preventDefault()
+
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var subject = document.getElementById('subject').value;
+  var message = document.getElementById('message').value;
+
+  alert('Hi ' + name + ', your message has been sent successfully. Enjoy the rest of your day.')
+
+  // var body = 'name: ' + name + '<br/> email: ' + email + '<br/> subject' + subject + '<br/> message' + message;
+
+  // Email.send({
+
+  //   From : "email",
+  //   Subject : subject,
+  //   Body : body
+  // }).then(
+  //   message => alert(message)
+  // );
+
+})
 
 // Toggler button animation
 const menuBtn = document.querySelector('.menuBtn');
