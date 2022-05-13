b595739a9232d33fcee84ecbb8d832faabf70134
Shaviya <91133853+ShaviyaVictor@users.noreply.github.com>
2022-05-13 22:25:53 +0300

    Update script.js
    Fixing the UX

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ toggleBtn.addEventListener('click', () => {
 
 // ==== typing script ====
 var typed = new Typed('.typing', {
-  strings:[' ', 'Town Planner', 'Graphic Designer', 'Web Designer', 'Full Stack Developer.'],
+  strings:[' ', 'Town Planner', 'Graphic Designer', 'Web Designer', 'Full Stack Developer'],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true 
